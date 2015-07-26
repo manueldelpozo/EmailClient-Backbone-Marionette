9b81f480d3ef8186378ca0c361da21ee8787acf6
manuel <mdp.webdeveloper@gmail.com>
2015-07-26 23:36:42 +0200

    Views and Collection of views

--- js/app.js
+++ js/app.js
@@ -5,5 +5,18 @@ var Mails = Backbone.Collection.extend({
   model: Mail
 });
 
-
-
+//VIEWS
+// View for mail content
+var ContentView = Backbone.Marionette.ItemView.extend({
+  template: '#contentView'
+});
+// View for mail list item
+var MailView = Backbone.Marionette.ItemView.extend({
+  className: 'unread',
+  template: '#mailView'
+});
+// Collection View 
+var MailsView = Backbone.Marionette.CollectionView.extend({
+  tagName: 'div',
+  childView: MailView
+});
